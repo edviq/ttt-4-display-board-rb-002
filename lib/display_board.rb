33b5602c08df36fd0a104c0ed298a889c771bdb2
edviq <elukyanov@gmail.com>
2015-10-31 17:13:58 -0400

    Board

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,30 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+
+
+
+def display_board(board)
+
+  board = [" "," "," "," "," "," "," "," "," "]
+
+puts "   |   |   " "-----------"
+puts "   | X |   " 
+puts " O |   |   " 
+
+puts " O |   |   " "   | X |   " 
+ 
+
+puts " X | X | X " 
+puts " O | O | O " 
+puts " X |   |   " "  | X |   " "  |   | X " 
+puts "   |   | O " "  | O |   " "  |   | O " 
+
+puts " X | X | X " " X | O | O " " X | O | O " 
+
+puts " X | O | X " " O | X | X " " O | X | O "
+
+
+end
+
+#display_board(board)
